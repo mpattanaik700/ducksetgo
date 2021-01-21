57206cfc09e140413b786ea0be9298d544c43555
Manoja Pattanaik <manoja.pattanaik@calsoftinc.com>
2021-01-21 21:25:14 +0530

    file encoding set

--- src/main/java/io/github/mpattanaik700/App.java
+++ src/main/java/io/github/mpattanaik700/App.java
@@ -6,6 +6,10 @@ package io.github.mpattanaik700;
  */
 public class App 
 {
+	/**
+	 * Entry point function
+	 * @param args input arguments
+	 */
     public static void main( String[] args )
     {
         System.out.println( "Hello World!!!" );
